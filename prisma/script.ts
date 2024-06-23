015e699febed92dfbb3a060bbc579af3b568ba3e
warlock <fecypher@gmail.com>
2024-06-23 16:53:09 +0100

    changed file

--- prisma/script.ts
+++ prisma/script.ts
@@ -1,19 +1,5 @@
-import { Pool, neonConfig } from "@neondatabase/serverless";
-import { PrismaNeon } from "@prisma/adapter-neon";
 import { PrismaClient } from "@prisma/client";
-import dotenv from "dotenv";
-import ws from "ws";
 
-declare global {
-  var prisma: PrismaClient | undefined;
-}
-
-dotenv.config();
-neonConfig.webSocketConstructor = ws;
-const connectionString = `${process.env.DATABASE_URL}`;
-
-const pool = new Pool({ connectionString });
-const adapter = new PrismaNeon(pool);
-const prisma = new PrismaClient({ adapter, log: ["info", "warn", "error"] });
+const prisma = new PrismaClient();
 
 export { prisma };
